679c5e426813fa802358ec5c59b872f8d671f19f
hondahee <heewhy3513@gmail.com>
2025-07-07 17:16:16 +0900

    초기 세팅

---
+++ src/main/java/com/example/rise/RiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rise/RiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
